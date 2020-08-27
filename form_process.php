e7ad6543f1988408c57796a3a482d217e026aec5
jellyownsyou <66040096+jellyownsyou@users.noreply.github.com>
2020-08-27 15:56:48 +0200

    f
    g

--- form_process.php
+++ form_process.php
@@ -1,16 +1,10 @@
 <?php
 
-if(isset($_POST['submit'])) {
-    
-    $email = $_POST['email'];
-    $to = "samo.goljat1@gmail.com";
-//    $subject = $_POST['country'];
-    $subject = "To je subject";
-    $text = $_POST['subject'];
-    $headers = "From: {$email}";
-        
-    mail($to,$subject,$text);
-    header("Location: ./domov.html");
+ini-set( 'display_errors', 1);
+error_reporting ( E_ALL );
+
+$from = "agencija@digitalsgmarketing.com";
+$to = ""
     
 }
 ?>
