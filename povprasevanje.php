d00abe761bb56eeb8c6022b0c7b9fb6da8b3237b
jellyownsyou <66040096+jellyownsyou@users.noreply.github.com>
2020-08-27 21:59:32 +0200

    h
    h

--- povprasevanje.php
+++ povprasevanje.php
@@ -1,3 +1,37 @@
+
+<?php
+
+$result="";
+if(isset($_POST['submit'])){
+    require 'phpmailer/PHPMailerAutoload.php';
+    $mail = new PHPMailer;
+
+    $mail->Host='smtp.gmail.com';
+    $mail->Port=587;
+    $mail->SMTPAuth=true;
+    $mail->SMTPSecure='tls';
+    $mail->Username='digitalsgmarketing@gmail.com';
+    $mail->Password='programersamo';
+
+    $mail->setFrom($_POST['email'], $_POST['name']);
+    $mail->addAddress(digitalsgmarketing@gmail.com);
+    $mail->addReplyTo($_POST['email'], $_POST['name']);
+    
+    $mail->isHTML(true);
+    $mail->Subject='Form Submission: '.$_POST['subject'];
+    $mail->Body='<h1 align=center'>Name :'.$_POST['name'].'<br>Email: '.$_POST['email'].'<br>
+        Message: '.$_POST['msg'].'</h1>;
+
+    if(!$mail->send()){
+        $result="Something went wrong. Please try again.";
+    }
+    else{
+        $result="Thanks ".$_POST['name']." for contacting us. We'll get back to you soon!"; }
+            }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -35,13 +69,16 @@
                 <br>
                 <form action="form_process.php">
                     <label for="fname">Ime</label>
-                    <input type="text" id="fname" name="firstname" placeholder="Vaše ime..">
+                    <input type="text" id="name" name="firstname" placeholder="Vaše ime..">
 
                     <label for="lname">Priimek</label>
-                    <input type="text" id="lname" name="lastname" placeholder="Vaš priimek..">
+                    <input type="text" id="lname" name="lastname" placeholder="Vaš priimek.."> 
+                    
+                    <label for="email">Email</label>
+                    <input type="text" id="email" name="email" placeholder="Vaš e-poštni naslov..">
                     
                     <label for="lname">Podjetje</label>
-                    <input type="text" id="lname" name="lastname" placeholder="Vaše podjetje..">
+                    <input type="text" id="email" name="lastname" placeholder="Vaše podjetje..">
 
                     <label for="country">Željena storitev</label>
                     <select id="country" name="country">
@@ -52,7 +89,7 @@
                     <label for="subject">Podrobnosti</label>
                     <textarea id="subject" name="subject" placeholder="Sporočilo.." style="height:200px"></textarea>
 
-                    <input type="submit" value="Oddaj">
+                    <input type="submit" name="submit" id="submit" value="Oddaj">
                 </form>
             </div>
         </div>
